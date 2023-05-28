d268c2cc8f3e6b1cedd31741c5a782fec8662e97
Kite <ixkaito@gmail.com>
2023-05-28 20:39:25 +0900

    Add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    fontSize: {
+      base: ['0.875rem', '1rem'],
+    },
+    extend: {
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
